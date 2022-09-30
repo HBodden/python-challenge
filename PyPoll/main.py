22358a1534dfc33f505a7b5f3b514d311373e210
Huntley Bodden <huntley.bodden@gmail.com>
2022-09-30 18:45:20 -0400

    Vote count solved

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,9 @@ import os
 import csv
 
 #Set a varribale for the csv file
-poll_csv = os.path.join("..","Resources","election_data.csv")
+poll_csv = os.path.join("Resources","election_data.csv")
+
+votecount = 0
 #Read in the CSV file 
 with open(poll_csv) as csvfile:
 
@@ -13,7 +15,33 @@ with open(poll_csv) as csvfile:
     print(header)
 
     for row in csv_reader:
-        print(row)
+        #print(row)
+        if int(row[0]) >= 0:        
+            votecount += 1
+    
+    
+print(votecount)
 
     
+# Election Results
+# -------------------------
+# Total Votes: 369711
+# -------------------------
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# -------------------------
+# Winner: Diana DeGette
+# -------------------------
+#Challenge output
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {votecount} ")
+print("-------------------------")
+print("Candidates are: ")
 
+
+
+print("-------------------------")
+print("Winner: ")
+print("-------------------------")
