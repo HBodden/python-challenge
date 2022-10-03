b1afc729a9c402e1a691d38b84cd1993f845748f
Huntley Bodden <huntley.bodden@gmail.com>
2022-10-03 13:31:02 -0400

    added comments to the code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,13 +19,14 @@ with open(poll_csv) as csvfile:
     
     header = next(csv_reader)
 #print(header)
-
+#count the number of rows / votes in the file 
     for row in csv_reader:
         #print(row)
         if int(row[0]) >= 0:        
             votecount += 1
 #Count the number of votes    
 #print(votecount)
+#identify the unique candidates names each candidate has a defined position in the list
 with open(poll_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     header2 = next(csv_reader)
@@ -33,6 +34,7 @@ with open(poll_csv) as csvfile:
         if name[2] not in cand_names:
             cand_names.append(name[2])
 #print(cand_names)
+#count the votes for each candidate 
 with open(poll_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     header2 = next(csv_reader)
@@ -46,12 +48,13 @@ with open(poll_csv) as csvfile:
 # print(cand1_count)
 # print(cand2_count)
 # print(cand3_count)
-
+#sort the votes moving the highest number of votes to the last position in the list
 votes = [cand1_count, cand2_count, cand3_count]
 sort_votes = sorted(votes)
 #print(votes)
 #print(sorted(votes))
 #print(sort_votes[len(sort_votes)-1])
+#Identify the winner of the election by matching the number of votes to the highest vote identifiy in the previous step
 t= False 
 while t == False:
     if sort_votes[len(sort_votes)-1] == votes[0]:
@@ -83,7 +86,6 @@ print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {votecount} ")
 print("-------------------------")
-print("The Candidates are: ")
 print(str(cand_names[0])+ ": " + str("{:.3%}".format(cand1_count/votecount))+ " " + "(" +str(cand1_count) + ")")
 print(str(cand_names[1])+ ": " + str("{:.3%}".format(cand2_count/votecount))+ " " + "("+str(cand2_count)+")")
 print(str(cand_names[2])+ ": " + str("{:.3%}".format(cand3_count/votecount))+ " " + "("+str(cand3_count)+")")
